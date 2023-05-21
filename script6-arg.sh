f6d545c3cd1b5ddce2d142fe612ac662f11afbab
SriSanthosh-Attili <santhosh.shmk@gmail.com>
2023-05-21 08:12:16 +0530

    New Scripts with Arguments

---
+++ script6-arg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Script Arguments List"
+echo "\$* value = $*"
+echo "\$@ value = $@"
+echo "\$0 value = $0"
+echo "\$1 value = $1"
+echo "\$2 value = $2"
+echo "\$3 value = $3"
+echo "\$4 value = $4"
+echo "\$5 value = $5"
+echo "\$# value = $# -- Total arguments count"
+echo "\$$ value = $$"
